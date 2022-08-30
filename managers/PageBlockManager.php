b36958d14f98d9ed54c2d67c99b982f7062d4daa
cecilejeanneau <cecile.n.jeanneau@gmail.com>
2022-08-30 15:22:48 +0000

    forgot to clean PageBlockManager

--- managers/PageBlockManager.php
+++ managers/PageBlockManager.php
@@ -24,46 +24,26 @@
             
         }
         
-        public function getPageBlockById(string $id): ?PageBlock {
-            // READ
-            $query = $this->db->prepare('SELECT name, page_id, media_id, page_block_type_id FROM page_blocks WHERE page_blocks.id = :id');
-            $parameters = [
-                'id' => $id
-            ];
-            $query->execute($parameters);
-            $result = $query->fetch(PDO::FETCH_ASSOC);
-            // returns the result in an associative array
+        // public function getPageBlockById(string $id): ?PageBlock {
+        //     // READ
+        //     $query = $this->db->prepare('SELECT name, page_id, media_id, page_block_type_id FROM page_blocks WHERE page_blocks.id = :id');
+        //     $parameters = [
+        //         'id' => $id
+        //     ];
+        //     $query->execute($parameters);
+        //     $result = $query->fetch(PDO::FETCH_ASSOC);
+        //     // returns the result in an associative array
             
-            try {
-                if(!is_null($result)) {
-                    return new PageBlock($result['id'], $result['name'], $result['page_id'],     $result['media_id'], $result['page_block_type_id']);
-                } else {
-                    throw new Exception("This page block doesn't exist");
-                }
-            } catch (Exception $e) {
-                echo "Exception : ".$e->getMessage();
-            }
-        }
-        
-        
-        function getMessageByRoom($messageRoom) :?array 
-{
-    $db = new PDO(
-    'mysql:host=db.3wa.io;port=3306;dbname=cecilejeanneau_distorsion_project',
-    'cecilejeanneau',
-    '32da58d8f8e9297ba228dd3b6a91430b'
-    );
-    
-    $query = $db->prepare('SELECT * FROM messages JOIN rooms ON rooms.id = messages.room_id WHERE rooms.name = :messageRoom');
-    $parameters = [
-        'messageRoom' => $messageRoom
-        // function parameter name the room where the messages are from
-    ];
-    $query->execute($parameters);
-    $result = $query->fetchAll(PDO::FETCH_ASSOC);
-    
-    return $result;
-}
+        //     try {
+        //         if(!is_null($result)) {
+        //             return new PageBlock($result['id'], $result['name'], $result['page_id'],     $result['media_id'], $result['page_block_type_id']);
+        //         } else {
+        //             throw new Exception("This page block doesn't exist");
+        //         }
+        //     } catch (Exception $e) {
+        //         echo "Exception : ".$e->getMessage();
+        //     }
+        // }
         
         
         public function getPageBlockById(string $id): ?PageBlock {
