e8b20417fa29314008d9d46d18876f811a2e1c5f
cecilejeanneau <cecile.n.jeanneau@gmail.com>
2022-08-30 09:00:44 +0000

    work on authentication for connect

--- controllers/AuthenticationController.php
+++ controllers/AuthenticationController.php
@@ -2,29 +2,44 @@
 
 require_once "./managers/UserManager.php";
 
+
     class AuthenticationController extends AbstractController {
         
         private UserManager $user;
         
-        public function __construct() {
-            $this->user = new UserManager();
+        public function index(): void {
+            $this->render($page = "log-in");
+            // method for child class form AbstractController : need to extends
+        }
+        
+        public function show(): void {
+            $this->render($page = "sign-up");
+            // method for child class form AbstractController : need to extends
         }
         
         public function connectUser(array $post = null): void {
             // array because that's JSON return from POST
-            if(!is_null($post) && isset($post["form"]) {
-                // if it's not null and submit
-                $username = $post["username"];
-                $password = $post["password"];
-                
-                if($username === $result['username'] && $password === $result['password'] ) {
-                    
-                    echo "welcome".$username.""."you're connected !";
-                    $routeFound = true;
-                    $request = "/";
+            
+                if(!is_null($_POST["username"]) && isset($_POST["username"])) {
+                    // if username is not null and set
+                    if(!is_null($_POST["password"]) && isset($_POST["password"]    )) {
+                        // if password is not null and set
+                        if(!is_null($_POST["email"]) && isset($_POST["email"]))     {
+                            // if email is not null and set
+                            $username = $post["username"];
+                            $password = $post["password"];
+                            $email = $post["email"];
+                            
+                            if($username === $result['username'] && $password === $result['password'] ) {
+                                
+                                echo "welcome".$username.""."you're connected !";
+                                $routeFound = true;
+                                $request = "/admin";
+                            }
+                        }
+                    }
                 }
-                
-            }
+            
             else
             {
                 echo "404 error !";   
--- managers/PageManager.php
+++ managers/PageManager.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "./managers/AbstractManager.php";
+// require_once "./managers/AbstractManager.php";
 
     class PageManager extends AbstractManager {
         
