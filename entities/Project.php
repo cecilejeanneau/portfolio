aaf9fac697e81fcc88bce0f1d6bedb4e2f487fbb
cecilejeanneau <cecile.n.jeanneau@gmail.com>
2022-09-09 10:09:12 +0000

    adding type hinting on Project setters

--- entities/Project.php
+++ entities/Project.php
@@ -37,23 +37,23 @@
         }
         // get informations and display them for example
         
-        public function setName() {
+        public function setName(): self {
             $this->name = $name;
             return $this;
         }
-        public function setResume() {
+        public function setResume(): self {
             $this->resume = $resume;
             return $this;
         }
-        public function setMediaId() {
+        public function setMediaId(): self {
             $this->media_id = $media_id;
             return $this;
         }
-        public function setPageTypeId() {
+        public function setPageTypeId(): self {
             $this->page_type_id = $page_type_id;
             return $this;
         }
-        public function setSlug() {
+        public function setSlug(): self {
             $this->slug = $slug;
             return $this;
         }
